c199015029dc3ec0538b17ccaa048f85fc93bdd9
Banel <banelbucknor@gmail.com>
2022-05-04 15:11:18 -0400

    add package.json files

--- src/App.js
+++ src/App.js
@@ -5,38 +5,14 @@ import { BASE_URL, RANDOM_EMOJI_URL } from './globals'
 import Emoji from './components/Emoji'
 import EmojiSymbols from './components/EmojiSymbols'
 
-
-
-
-
-
-
-
-
-
-const App = () => {
-  const [displayArt, setDisplayArt] = useState(false)
-
-  const toggleArt = () => {
-    displayArt ? setDisplayArt(false) : setDisplayArt(true)
+useEffect(() => {
+  const getExcuses = async () => {
+    const response = await axios.get(`${ALL_EXCUSES_URL}`)
+    // console.log(response.data)
+    setExcuses(response.data)
   }
-
-  return (
-    <div className="App">
-      <h1>World of Art Works</h1>
-      <main>
-        <div className="art-container">
-          {displayArt ? (
-            <ArtWorks displayArt={displayArt} />
-          ) : (
-            <h2>See Some ArtWorks</h2>
-          )}
-
-{artworksArray.map((artworks, index) => (
-        <div className="artworks" key={artworks.id}>
-            <ArtWorks artworks={artworks} key={artworks.id}/>
-        </div>
-      ))}
+  getExcuses()
+}, [])
 
 
 
@@ -44,16 +20,47 @@ const App = () => {
 
 
 
-        </div>
 
-        <button onClick={toggleArt}>
-          {displayArt ? 'Clear Art' : 'New Art'}
-        </button> 
-      </main>
+// const App = () => {
+//   const [displayArt, setDisplayArt] = useState(false)
 
-    </div>
-  )
-}
+//   const toggleArt = () => {
+//     displayArt ? setDisplayArt(false) : setDisplayArt(true)
+//   }
+
+//   return (
+//     <div className="App">
+//       <h1>World of Art Works</h1>
+//       <main>
+//         <div className="art-container">
+//           {displayArt ? (
+//             <ArtWorks displayArt={displayArt} />
+//           ) : (
+//             <h2>See Some ArtWorks</h2>
+//           )}
+
+// {artworksArray.map((artworks, index) => (
+//         <div className="artworks" key={artworks.id}>
+//             <ArtWorks artworks={artworks} key={artworks.id}/>
+//         </div>
+//       ))}
+
+
+
+
+
+
+
+//         </div>
+
+//         <button onClick={toggleArt}>
+//           {displayArt ? 'Clear Art' : 'New Art'}
+//         </button> 
+//       </main>
+
+//     </div>
+//   )
+// }
 
 export default App
 
