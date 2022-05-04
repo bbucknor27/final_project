04ea1b64d6565be9403935189b5a69db66ddafd6
Banel <banelbucknor@gmail.com>
2022-05-04 14:01:54 -0400

    using a new api: emojihub

---
+++ src/globals.js
@@ -0,0 +1,7 @@
+
+export const BASE_URL = 'https://emojihub.herokuapp.com/api/all'
+
+export const RANDOM_EMOJI_URL = 'https://emojihub.herokuapp.com/api/random'
+
+// export const API_KEY = process.env.REACT_APP_API_KEY 
+
